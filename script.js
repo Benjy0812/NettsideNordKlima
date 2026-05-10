a4efae2c0234ca234227cd93ac387f37099921b1
Benjy <108727379+Benjy0812@users.noreply.github.com>
2026-05-10 02:03:16 +0200

    WIP working on getting and displaying data from fetchWeather function

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ async function fetchWeather(lat, lon) {
   if (!response.ok) throw new Error('Failed to fetch weather')
   return await response.json()
 }
+
+function displayCurrentWeather(data) {
+    const current = data.properties.timeseries[0];
+    const { air_pressure_at_sea_level, air_temperature, cloud_area_fraction, relative_humidity, wind_from_direction, wind_speed } = current.data.instant.details;
+    const symbolCode = current.data.next_1_hours.summary.symbol_code;
+}
