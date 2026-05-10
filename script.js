7b6dc587751db11b4bfd2e324cb28266f8c0dea7
Benjy <108727379+Benjy0812@users.noreply.github.com>
2026-05-11 00:24:18 +0200

    Feature: Get button now wroks and displays temp Etc, and small fixes

--- script.js
+++ script.js
@@ -16,4 +16,25 @@ function displayCurrentWeather(data) {
     wind_speed,
   } = current.data.instant.details
   const symbolCode = current.data.next_1_hours.summary.symbol_code
+
+  document.getElementById('temp').textContent = `Temp: ${air_temperature} °C`
+  document.getElementById('pressure').textContent =
+    `Pressure: ${air_pressure_at_sea_level} hPa`
+  document.getElementById('humidity').textContent =
+    `Humidity: ${relative_humidity} %`
+  document.getElementById('clouds').textContent =
+    `Clouds: ${cloud_area_fraction} %`
+  document.getElementById('wind').textContent =
+    `Wind: ${wind_speed} m/s from ${wind_from_direction}°`
+  document.getElementById('symbol').textContent = `Weather: ${symbolCode}`
 }
+
+const fetchButton = document.getElementById('fetch-weather')
+
+fetchButton.addEventListener('click', async () => {
+  let lat = document.getElementById('lat').value
+  let lon = document.getElementById('lon').value
+
+  const data = await fetchWeather(lat, lon)
+  displayCurrentWeather(data)
+})
