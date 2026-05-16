d8b2b30d7700febbeebe0e0e4acd6268b3e0d3f3
Benjy <108727379+Benjy0812@users.noreply.github.com>
2026-05-16 22:06:54 +0200

    Rename Base to reprecent github repo name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   // Uncomment the line below if you want to deploy to GitHub Pages or a subdirectory
-  //base: "/WebNordicWeather/",
+  //base: "/NettsideNordKlima/",
   server: {
     watch: {
       usePolling: true, // Optional: for WSL2/Docker
