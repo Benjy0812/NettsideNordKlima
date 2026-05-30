5dbbf81c0a77e5c9b2bd5351f32ed83fc2d14e41
Benjy <108727379+Benjy0812@users.noreply.github.com>
2026-05-31 01:10:10 +0200

    Add temperature unit toggle and wind arrow canvas

--- src/ui.js
+++ src/ui.js
@@ -27,4 +27,48 @@ export function displayCurrentWeather(data) {
   document.getElementById('wind').textContent =
     `Vind: ${wind_speed} m/s fra ${wind_from_direction}°`
   document.getElementById('symbol').textContent = `Vær: ${symbolCode}`
+
+  updateTemperature(air_temperature)
+
+  const tempToggle = document.getElementById('temp-toggle')
+  tempToggle.addEventListener('change', () => {
+    updateTemperature(air_temperature)
+  })
+  const canvas = document.getElementById('arrow')
+  const ctx = canvas.getContext('2d')
+  arrowDirection(
+    ctx,
+    canvas.width / 2,
+    canvas.height / 2,
+    wind_from_direction,
+    100,
+  )
+}
+
+export function arrowDirection(ctx, cx, cy, wind_from_direction, length) {
+  const blowTo = (wind_from_direction + 180) % 360
+
+  // convert degress to radiance
+  const rad = (blowTo * Math.PI) / 180
+
+  const horizontal = cx + Math.sin(rad) * length
+  const vertical = cy + Math.cos(rad) * length
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+  ctx.beginPath()
+  ctx.moveTo(cx, cy)
+  ctx.lineTo(horizontal, vertical)
+  ctx.stroke()
+}
+
+// Update the temperature display based on the selected unit (Celsius or Fahrenheit)
+export function updateTemperature(air_temperature) {
+  const tempC = air_temperature
+  const tempToggle = document.getElementById('temp-toggle')
+  if (tempToggle.value === 'Celsius') {
+    document.getElementById('temp').textContent = `Temperatur: ${tempC} °C`
+  } else {
+    const tempF = (tempC * 9) / 5 + 32
+    tempF.toFixed(2)
+    document.getElementById('temp').textContent = `Temperatur: ${tempF} °F`
+  }
 }
